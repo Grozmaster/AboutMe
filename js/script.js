30b6c5d10bffba94a742037490d9f34d1d903014
Oleg Chaika <grozmaster@ukr.net>
2022-08-31 22:28:36 +0300

    fix path lang

--- js/script.js
+++ js/script.js
@@ -1,49 +1,39 @@
-const smoothLinks = document.querySelectorAll("a[href^='.']");
-for (let smoothLink of smoothLinks) {
-    smoothLink.addEventListener("click", function (e) {
-        e.preventDefault();
-        const block = smoothLink.getAttribute("href");
+// const smoothLinks = document.querySelectorAll("a[href^='.']");
+// for (let smoothLink of smoothLinks) {
+//     smoothLink.addEventListener("click", function (e) {
+//         e.preventDefault();
+//         const block = smoothLink.getAttribute("href");
 
-        document.querySelector(block).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
-    });
-};
-
-
-mybutton = document.getElementsByClassName('up__button');
-
-window.onscroll = function () { scrollFunction() };
-
-function scrollFunction() {
-    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-        mybutton[0].style = "display: block";
-    } else {
-        mybutton[0].style = "display: none";
-    }
-};
-
-const pop = document.querySelector('.popup');
-
-function popup() {
-    pop.classList.add('active');
-};
-
-function popdown() {
-    pop.classList.remove('active');
-
-};
+//         document.querySelector(block).scrollIntoView({
+//             behavior: "smooth",
+//             block: "start"
+//         });
+//     });
+// };
 
 
+// mybutton = document.getElementsByClassName('up__button');
 
+// window.onscroll = function () { scrollFunction() };
 
+// function scrollFunction() {
+//     if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+//         mybutton[0].style = "display: block";
+//     } else {
+//         mybutton[0].style = "display: none";
+//     }
+// };
 
+// const pop = document.querySelector('.popup');
 
+// function popup() {
+//     pop.classList.add('active');
+// };
 
-const change = document.querySelector(".lang__change");
+// function popdown() {
+//     pop.classList.remove('active');
 
-// change.addEventListener("click", setLang);
+// };
 
 
 
@@ -51,12 +41,9 @@ const change = document.querySelector(".lang__change");
 
 
 
-function setLang() {
-    location.href = window.location.pathname + '#' + change.textContent;
+function setLang(text) {
+    location.href = window.location.pathname + '#' + text;
     location.reload();
-
-
 }
-
 
 
